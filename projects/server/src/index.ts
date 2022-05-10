9a293f7f7ac8de142bb6d7db5d41683b0603827b
bibek@dzangolab <bibek@dzangolab.com>
2022-05-10 12:27:27 +0545

    feat: add express setup

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,8 +1,13 @@
+import cors from "cors"
 import express from "express"
 
 import config from "../config"
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(config.port, () => {
     console.log(`server started succesfully at port ${config.port}`)
