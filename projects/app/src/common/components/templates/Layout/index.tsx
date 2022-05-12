f5534476adcfbf1505da4e6b5d5a05c194d83150
bibek@dzangolab <bibek@dzangolab.com>
2022-05-12 12:10:02 +0545

    fix: fix css for different screens

--- projects/app/src/common/components/templates/Layout/index.tsx
+++ projects/app/src/common/components/templates/Layout/index.tsx
@@ -6,9 +6,21 @@ import styles from "./index.module.scss"
 export default function MainLayout(): JSX.Element {
     return (
         <section className={styles.topContainer}>
-            <h1>Navbar</h1>
-            <Outlet></Outlet>
-            <h2>Footer</h2>
+            <header>
+                <section>
+                    <h1>Logo</h1>
+                    <p>Search</p>
+                    <section className={styles.loginContainer}>
+                        <p>Login</p>
+                        <p>Signup</p>
+                    </section>
+                </section>
+            </header>
+            <section className={styles.layoutContainer}>
+                <h1>Navbar</h1>
+                <Outlet></Outlet>
+                <h2>Footer</h2>
+            </section>
         </section>
     )
 }
